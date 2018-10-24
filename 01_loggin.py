6558aef240f6e4044509049edca748be689b898a
Richard Xie <defaulttest@sina.com>
2018-10-24 06:51:34 -0400

    测试log_demo,与此项目无关

---
+++ 01_loggin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+logging.basicConfig(level=logging.WARNING)  # 配置默认等级
+
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")

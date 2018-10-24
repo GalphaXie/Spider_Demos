c6a4ac2d1a5bf2a5729f96300d4f7fb8f1afb133
Richard Xie <defaulttest@sina.com>
2018-10-24 02:06:45 -0400

    管道的封装

---
+++ MyScrapy/scrapy_plus/core/pipeline.py
@@ -0,0 +1,14 @@
+# 管道对象的封装
+
+
+class Pipeline:
+    """完成对管道对象的封装, 负责处理item数据"""
+
+    def process_item(self, item):
+        """
+        对item进行处理
+        :param item:
+        :return:
+        """
+        print('item: ', item)
+        return item
